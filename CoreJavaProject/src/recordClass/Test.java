46c2978a89c5134a26f3183621d705c3b422d43e
Vasari123 <ashokvasari94@gmail.com>
2025-09-04 21:51:04 +0530

    added adjusted format

--- CoreJavaProject/src/recordClass/Test.java
+++ CoreJavaProject/src/recordClass/Test.java
@@ -3,15 +3,14 @@ package recordClass;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
 
         List<Employee> list = Arrays.asList(
-                new Employee(1,"Ashok",10.0),
-                new Employee(2,"Naveen",50.0),
-                new Employee(3,"Naresh",15.0)
+                new Employee(1, "Ashok", 10.0),
+                new Employee(2, "Naveen", 50.0),
+                new Employee(3, "Naresh", 15.0)
         );
 
         List<Employee> sortedSalary = list.stream().sorted(Comparator.
