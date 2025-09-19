ea9f336ce26f1d0ca111a4879705b9f656e13ddd
Vasari123 <ashokvasari94@gmail.com>
2025-09-19 22:18:48 +0530

    added thread safe singleton

---
+++ CoreJavaProject/src/singletonClass/Singleton.java
@@ -0,0 +1,23 @@
+package singletonClass;
+
+public class Singleton {
+    private static Singleton singleton;
+
+    private Singleton(){
+        System.out.println("Singleton instance created by " + Thread.currentThread().getName());
+    }
+
+    public static Singleton getSingleton(){
+        if (singleton == null){
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+    public void showMessage(){
+        System.out.println("Hello from Singleton! " + Thread.currentThread().getName());
+    }
+}
---
+++ CoreJavaProject/src/singletonClass/SingletonTest.java
@@ -0,0 +1,14 @@
+package singletonClass;
+
+public class SingletonTest {
+    public static void main(String[] args) {
+        Runnable runnable = () ->{
+            Singleton singleton = Singleton.getSingleton();
+            singleton.showMessage();
+        };
+        Thread thread1 = new Thread(runnable, "Task-1");
+        Thread thread2 = new Thread(runnable, "Task-2");
+        thread1.start();
+        thread2.start();
+    }
+}
